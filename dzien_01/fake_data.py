d2f48aeb865d654e282849090de08ac308404bca
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-08-20 09:39:52 +0200

    Dzien_02 - dane fake

--- dzien_01/fake_data.py
+++ dzien_01/fake_data.py
@@ -4,11 +4,11 @@ fake = Faker(["pl_PL", "de_DE"])
 
 tablica = []
 for _ in range(200):
-    wiersz = [fake.name(), fake.address(), fake.ascii_company_email(), fake.phone_number()]
+    wiersz = [fake.name(), fake.android_platform_token() , fake.ascii_company_email(), fake.phone_number()]
     tablica.append(wiersz)
     print(wiersz)
 
 # Tworzenie pliku CSV otwieranego przez Excela
-with open('dane.csv', 'w', newline='', encoding='utf-8') as plik:
+with open('../dzien_02/dane.csv', 'w', newline='', encoding='utf-8') as plik:
     writer = csv.writer(plik)
     writer.writerows(tablica)
