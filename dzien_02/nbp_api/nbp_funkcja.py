d9a598417f3c37c60b224d9f193a2eaa557a6bf6
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-08-20 15:08:28 +0200

    Dzien_02 - funkcje i parametry

--- dzien_02/nbp_api/nbp_funkcja.py
+++ dzien_02/nbp_api/nbp_funkcja.py
@@ -1,16 +1,21 @@
 import requests
 
-api_url = "https://api.nbp.pl/api/exchangerates/tables/a/?format=json"
+def import_from_nbp(kod_waluty='USD'):
+    """
 
-r = requests.get(api_url)
-dane = r.json()[0]
-
-print(f"{dane['effectiveDate']=}")
-lista_walut = dane['rates']
-print(lista_walut[:3])
-for waluta_rate in lista_walut:
-    if waluta_rate['code'] == "USD":
-        print(f"{waluta_rate=}")
-
-    if waluta_rate['code'] in szukane_waluty:
-        print(f"Szukana {waluta_rate=} / {waluta_rate['mid']=} zł")
+    :param waluta: kod waluty, default="USD
+    :return: dane_waluty dict
+    """
+    api_url = "https://api.nbp.pl/api/exchangerates/tables/a/?format=json"
+    r = requests.get(api_url)
+    dane = r.json()[0]
+    data_tabeli = dane['effectiveDate']
+    nr_tabeli = dane['no']
+    lista_walut = dane['rates']
+    kod_waluty = kod_waluty.upper()
+    for waluta_rate in lista_walut:
+        if waluta_rate['code'] == kod_waluty:
+            cur = waluta_rate['currency']
+            mid = waluta_rate['mid']
+            return (data_tabeli, nr_tabeli, cur, mid)
+    return None
