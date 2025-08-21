dc10028f6a5a9f78266aeda3596800f685f1d295
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-08-21 09:03:02 +0200

    dzien_03 - start

---
+++ dzien_03/nbp/nbp_funkcja.py
@@ -0,0 +1,27 @@
+import requests
+
+def import_from_nbp(kod_waluty='USD'):
+    """
+
+    :param waluta: kod waluty, default="USD
+    :return: dane_waluty dict
+    """
+    api_url = "https://api.nbp.pl/api/exchangerates/tables/a/?format=json"
+    r = requests.get(api_url)
+    dane = r.json()[0]
+    data_tabeli = dane['effectiveDate']
+    nr_tabeli = dane['no']
+    lista_walut = dane['rates']
+    kod_waluty = kod_waluty.upper()
+    for waluta_rate in lista_walut:
+        if waluta_rate['code'] == kod_waluty:
+            cur = waluta_rate['currency']
+            mid = waluta_rate['mid']
+            return (data_tabeli, nr_tabeli, cur, mid)
+    return None
+
+if __name__ == '__main__':
+    dane = import_from_nbp()
+    print(dane)
+    chf = import_from_nbp("chf")
+    print(chf)
---
+++ dzien_03/nbp/tworz_md_import.py
@@ -0,0 +1,35 @@
+import snakemd
+from datetime import datetime
+import subprocess
+
+from nbp_funkcja import import_from_nbp
+
+doc = snakemd.new_doc()
+waluta = 'gep'
+nr_tabeli = None
+data_tabeli = None
+kurs = None
+waluta_nazwa = None
+data_tabeli, nr_tabeli, waluta_nazwa, kurs = import_from_nbp(waluta)
+nazwa_md = f"plik_{waluta}_{data_tabeli}"
+plik_md = f"{nazwa_md}.md"
+plik_docx = f"{nazwa_md}.docx"
+polecenie = ["pandoc", "-o", plik_docx, plik_md ]
+
+doc.add_heading("RAPORT stanu kursu waluty", level=2)
+doc.add_horizontal_rule()
+doc.add_heading(f"Waluta: {waluta}", level=3)
+doc.add_raw(f"""
+W tabeli NBP nr: {nr_tabeli}
+
+w dniu: {datetime.now().date()}
+
+z dnia {data_tabeli}
+
+kurs 1 {waluta_nazwa} wynosi {kurs} zł.
+""")
+doc.add_horizontal_rule()
+
+doc.dump(nazwa_md)
+
+subprocess.run(polecenie)
