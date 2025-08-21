2e18a237947290add0865134cb0e56279bf1b8da
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-08-21 15:12:23 +0200

    dzien_03 - oop

--- dzien_03/oop/oop_2.py
+++ dzien_03/oop/oop_2.py
@@ -6,10 +6,13 @@ class Statek:
     def opisz(self):
         print(f"Nasz statek jest rodzaju {self.rodzaj} i nazywa się {self.nazwa}")
 
+    def odbijamy(self):
+        print(f"Nasz statek odbijamy od brzegu -> {self.nazwa}")
+
 
 class Zaglowiec(Statek):
     def __init__(self, nazwa, ile_masztow):
-        super().__init__(nazwa, "Zaglowiec") #### Metoda SUPER!!!!!!
+        super().__init__(nazwa, "Żaglowiec") #### Metoda SUPER!!!!!!
         self.masztow = ile_masztow
 
     def opisz(self):
@@ -19,10 +22,13 @@ class Zaglowiec(Statek):
 
 
 
-statek_1 = Statek("Anna Maria", "Zaglowiec")
+statek_1 = Statek("Anna Maria", "Zaglowiecccccccc")
 statek_2 = Statek("Sołdek", "Towarowy")
 statek_3 = Zaglowiec("Cutty Sark", 5)
 
 statek_1.opisz()
 statek_2.opisz()
+statek_1.odbijamy()
+statek_2.odbijamy()
 statek_3.opisz()
+statek_3.odbijamy()
