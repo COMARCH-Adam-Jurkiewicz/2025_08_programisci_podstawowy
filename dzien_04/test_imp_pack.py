5a466561342a0efade02765c72ca530ef4dcebec
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-08-22 13:43:42 +0200

    dzien_04

---
+++ dzien_04/test_imp_pack.py
@@ -0,0 +1,6 @@
+from funkcje_adama.funkcje_plikowe import dziwna_funkcja
+from time import sleep
+
+
+print(dziwna_funkcja())
+sleep(2)
---
+++ funkcje_adama/funkcje_plikowe.py
@@ -0,0 +1,9 @@
+def plik_parsuje_nazwe(nazwa:str)->str:
+    return nazwa[:-3]+"AAA"
+
+def dziwna_funkcja()->int:
+    """
+
+    :return: zawsze 1
+    """
+    return 1
